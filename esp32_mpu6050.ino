37baf3020a3da41c1d1a15722025d4655b96219d
Tz-Ai <92517578+Tz-Ai@users.noreply.github.com>
2026-02-20 17:00:16 +0530

    esp32_mpu6050.ino

--- esp32_mpu6050.ino
+++ esp32_mpu6050.ino
@@ -10,31 +10,26 @@ void setup() {
   Wire.begin(19, 22);     // SDA, SCL
   Wire.setClock(100000); // Safe I2C speed
 
-  Serial.println("Initializing MPU6050...");
   byte status = mpu.begin();
-
   if (status != 0) {
-    Serial.print("MPU init failed with code ");
     Serial.println(status);
     while (1);
   }
 
-  Serial.println("✅ MPU6050 Found!");
-  Serial.println("Calibrating...");
   delay(1000);
   mpu.calcOffsets(true, true); // gyro + accel
-  Serial.println("Calibration done");
 }
 
 void loop() {
   mpu.update();
 
-  Serial.print("Pitch: ");
-  Serial.print(mpu.getAngleX());
-  Serial.print(" | Roll: ");
-  Serial.print(mpu.getAngleY());
-  Serial.print(" | Yaw: ");
-  Serial.println(mpu.getAngleZ());
+  // CSV output: Pitch,Roll
+  Serial.print(mpu.getAccX());
+  Serial.print(",");
+  Serial.print(mpu.getAccY());
+  Serial.print(",");
+  Serial.println(mpu.getAccZ());
+
 
   delay(200);
 }
